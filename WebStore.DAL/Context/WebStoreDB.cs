a2f1966e2185c3cce8f5125828ee8e169ef93177
DenZone1 <dissap95-d@yandex.ru>
2022-06-18 15:27:42 +0400

    Add project files.

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Domain.Entities.Orders;
+
+namespace WebStore.DAL.Context;
+
+public class WebStoreDB : IdentityDbContext<User, Role, string>
+{
+    public DbSet<Product> Products { get; set; } = null!;
+
+    public DbSet<Brand> Brands { get; set; } = null!;
+
+    public DbSet<Section> Sections { get; set; } = null!;
+
+    public DbSet<Employee> Employees { get; set; } = null!;
+
+    public DbSet<Order> Orders { get; set; } = null!;
+
+    //public DbSet<OrderItem> OrderItems { get; set; } = null!;
+
+    public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options)
+    {
+        
+    }
+
+    //protected override void OnModelCreating(ModelBuilder db)
+    //{
+    //    base.OnModelCreating(db);
+
+    //    //db.Entity<Section>()
+    //    //   .HasMany(s => s.Products)
+    //    //   .WithOne(p => p.Section)
+    //    //   .OnDelete(DeleteBehavior.Cascade);
+    //}
+}
