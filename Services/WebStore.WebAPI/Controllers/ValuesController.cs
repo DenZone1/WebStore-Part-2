659c3596990ff93f22d77b0bbfdd7dcb227254fc
DenZone1 <dissap95-d@yandex.ru>
2022-06-18 17:16:45 +0400

    Заготовка контреллера тестолвого WebApi

---
+++ Services/WebStore.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers;
+
+[ApiController]
+//[Route("api/values")]
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+    private static readonly Dictionary<int, string> _Values = Enumerable.Range(1,10)
+        .Select(i =>(Id:i, Value:$"Valuee - {i}")).ToDictionary(v =>v.Id, v =>v.Value);
+}
