9587153c2f2a7f2421cd41355097269ca86e01be
DenZone1 <dissap95-d@yandex.ru>
2022-06-18 18:01:35 +0400

    Интерфейс клиента тестового WebAPI

---
+++ Services/WebStore.Interfaces/TestAPI/IValuesService.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace WebStore.Interfaces.TestAPI;
+
+public interface IValuesService
+{
+    IEnumerable<string> GetValues();
+
+    string GetById(int Id);
+
+    void Add(string value);
+
+    void Edit(int Id, string value);
+
+    bool Delete(int Id);
+}
