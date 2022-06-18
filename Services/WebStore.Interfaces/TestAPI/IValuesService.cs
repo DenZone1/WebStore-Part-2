0649787538b43b5d18296d82bef4a23e050a86ae
DenZone1 <dissap95-d@yandex.ru>
2022-06-18 18:24:30 +0400

    Логика клиента для тестового WebAPI

--- Services/WebStore.Interfaces/TestAPI/IValuesService.cs
+++ Services/WebStore.Interfaces/TestAPI/IValuesService.cs
@@ -6,7 +6,7 @@ public interface IValuesService
 {
     IEnumerable<string> GetValues();
 
-    string GetById(int Id);
+    string? GetById(int Id);
 
     void Add(string value);
 
--- Services/WebStore.WebAPI.Clients/Values/ValuesClient.cs
+++ Services/WebStore.WebAPI.Clients/Values/ValuesClient.cs
@@ -1,39 +1,70 @@
-﻿using WebStore.Interfaces.TestAPI;
+﻿using System.Net;
+using System.Net.Http.Json;
+
+using Newtonsoft.Json.Linq;
+
+using WebStore.Interfaces.TestAPI;
 using WebStore.WebAPI.Clients.Base;
 
 namespace WebStore.WebAPI.Clients.Values;
 
 public class ValuesClient : BaseClient, IValuesService
 {
-    public ValuesClient(HttpClient Client) : base(Client,"api/values") 
+    public ValuesClient(HttpClient Client) : base(Client, "api/[controller]") 
     {
 
     }
 
     public IEnumerable<string> GetValues()
     {
-        throw new NotImplementedException();
+        var response = Http.GetAsync(Address).Result;
+
+        if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+            return Enumerable.Empty<string>();
+
+        if (response.IsSuccessStatusCode)
+            return response.Content.ReadFromJsonAsync<IEnumerable<string>>().Result!;
+
+        return Enumerable.Empty<string>();
+    }
+
+
+    public string GetById(int Id)
+    {
+        var response = Http.GetAsync($"{Address}/{Id}").Result;
+
+        if (response.IsSuccessStatusCode)
+            return response.Content.ReadFromJsonAsync<string>().Result!;
+
+        return null;
     }
 
     public void Add(string value)
     {
-        throw new NotImplementedException();
+        var response = Http.PostAsJsonAsync(Address, value).Result;
+        response.EnsureSuccessStatusCode();
+
     }
 
     public bool Delete(int Id)
-    {
-        throw new NotImplementedException();
+{
+        var response = Http.DeleteAsync($"{Address}/{Id}").Result;
+
+        if(response.IsSuccessStatusCode)
+            return true;
+        if (response.StatusCode == HttpStatusCode.NotFound)
+            return false;
+
+        response.EnsureSuccessStatusCode();
+        throw new InvalidOperationException();
     }
 
     public void Edit(int Id, string value)
     {
-        throw new NotImplementedException();
+        var response = Http.PutAsJsonAsync($"{Address}/{Id}", value).Result;
+        response.EnsureSuccessStatusCode();
     }
 
-    public string GetById(int Id)
-    {
-        throw new NotImplementedException();
-    }
-
+   
     
 }
