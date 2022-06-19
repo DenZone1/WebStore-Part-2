dc0024af517d42392019761872d3f91d4b56fbf0
DenZone1 <dissap95-d@yandex.ru>
2022-06-19 14:42:53 +0400

    Добавления возможности отмены ассинхронных операций

--- Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -17,9 +17,9 @@ public abstract class BaseClient
 
     protected T?  Get<T>(string url) => GetAsync<T>(url).Result;
 
-    protected async Task<T?> GetAsync<T>(string url)
+    protected async Task<T?> GetAsync<T>(string url, CancellationToken Cancel = default)
     {
-        var response = await Http.GetAsync(url).ConfigureAwait(false);
+        var response = await Http.GetAsync(url, Cancel).ConfigureAwait(false);
 
         //if (response.StatusCode == HttpStatusCode.NoContent)
         //    return default;
@@ -41,30 +41,30 @@ public abstract class BaseClient
                 default:
                 var result = await response
                     .EnsureSuccessStatusCode()
-                    .Content.ReadFromJsonAsync<T>();
+                    .Content.ReadFromJsonAsync<T>(cancellationToken : Cancel);
                 return result;
         }
     }
 
 
     protected HttpResponseMessage Post<T>(string url, T value) => PostAsync<T>(url, value).Result;
-    protected async Task<HttpResponseMessage> PostAsync<T>(string url, T value)
+    protected async Task<HttpResponseMessage> PostAsync<T>(string url, T value, CancellationToken Cancel = default)
     {
-        var response = await Http.PostAsJsonAsync(url, value).ConfigureAwait(false);
+        var response = await Http.PostAsJsonAsync(url, value,Cancel).ConfigureAwait(false);
         return response.EnsureSuccessStatusCode();
     }
 
     protected HttpResponseMessage Put<T>(string url, T value) => PutAsync<T>(url, value).Result;
-    protected async Task<HttpResponseMessage> PutAsync<T>(string url, T value)
+    protected async Task<HttpResponseMessage> PutAsync<T>(string url, T value, CancellationToken Cancel = default)
     {
-        var response = await Http.PutAsJsonAsync(url, value).ConfigureAwait(false);
+        var response = await Http.PutAsJsonAsync(url, value, Cancel).ConfigureAwait(false);
         return response.EnsureSuccessStatusCode();
     }
 
     protected HttpResponseMessage Delete<T>(string url) => DeleteAsync(url).Result;
-    protected async Task<HttpResponseMessage> DeleteAsync(string url)
+    protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken Cancel = default)
     {
-        var response = await Http.DeleteAsync(url).ConfigureAwait(false);
+        var response = await Http.DeleteAsync(url, Cancel).ConfigureAwait(false);
         return response;
     }
 }
