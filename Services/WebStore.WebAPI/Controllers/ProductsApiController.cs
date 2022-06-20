fb8b327cdddf67ce5bfe1b6b8fff7d8a4c9db553
DenZone1 <dissap95-d@yandex.ru>
2022-06-20 21:14:50 +0400

    Заготовка контроллера товаров

---
+++ Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductsApiController : ControllerBase
+{
+    private readonly IProductData _producData;
+    private readonly ILogger<ProductsApiController> _logger;
+
+    public ProductsApiController(IProductData ProducData, ILogger<ProductsApiController> Logger)
+    {
+        _producData = ProducData;
+        _logger = Logger;
+    }
+}
