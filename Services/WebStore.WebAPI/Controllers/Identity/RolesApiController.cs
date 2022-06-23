5431801b5126e8633a1b2c564ba55ec9b7317511
DenZone1 <dissap95-d@yandex.ru>
2022-06-23 20:46:12 +0400

    Заготовка контроллеров WebAPI для Identity

---
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -0,0 +1,25 @@
+﻿
+using WebStore.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WebStore.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebStore.WebAPI.Controllers.Identity;
+
+[ApiController]
+[Route("api/roles")]
+public class RolesApiController : ControllerBase
+{
+    private readonly RoleStore<Role> _RoleStore;
+    private readonly ILogger<RolesApiController> _logger;
+
+    public RolesApiController(WebStoreDB db, ILogger<RolesApiController> Logger)
+    {
+        _logger = Logger;
+        _RoleStore = new(db);
+    }
+
+    [HttpGet("all")]
+    public async Task<IEnumerable<Role>> GetAll() => await _RoleStore.Roles.ToArrayAsync();
+}
