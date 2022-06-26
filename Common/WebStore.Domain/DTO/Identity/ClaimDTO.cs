443af6a8acba58b7c2bbce81e759abaa085409b0
DenZone1 <dissap95-d@yandex.ru>
2022-06-26 12:24:22 +0400

    DTO модели объектов операций Identity

---
+++ Common/WebStore.Domain/DTO/Identity/ClaimDTO.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Security.Claims;
+
+namespace WebStore.Domain.DTO.Identity;
+
+public class ClaimDTO : UsedDTO
+{
+    public IEnumerable<Claim> Claims { get; init; } = null!;
+}
+public class ReplaceClainDTO : UsedDTO
+{
+    public Claim Claim { get; init; } = null!;
+    public Claim NewClaim { get; init; } = null!;
+}
---
+++ Common/WebStore.Domain/DTO/Identity/UsedDTO.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Domain.DTO.Identity;
+
+public abstract class UsedDTO
+{
+    public User User { get; set; } = null!;
+}
+
+public class AddLoginDTO : UsedDTO
+{
+    public UserLoginInfo UserLoginInfo { get; init; } = null!;
+}
+
+public class PasswordHashDTO : UsedDTO
+{
+    public string Hash { get; set; } = null!;
+}
+
+public class SetLockoutDTO : UsedDTO
+{
+    public DateTimeOffset? LockoutEnd { get; init; } = null!;
+}
