0167d96f6eb588a3bcff4576c44e5b2cd12c3b86
DenZone1 <dissap95-d@yandex.ru>
2022-06-26 17:13:52 +0400

    Конфигурация системы логирования

---
+++ Services/WebStore.WebAPI/Controllers/ConsoleAPIControoler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers;
+[ApiController]
+[Route("api/console")]
+public class ConsoleAPIControoler : ControllerBase
+{
+    [HttpGet("clear")]
+    public void Clear() => Console.Clear();
+
+    [HttpGet("write")]
+    [HttpGet("write({str})")]
+    public void Write(string str) => Console.WriteLine();
+}
