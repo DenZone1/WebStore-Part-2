9d082538855287c39b3c591d1e91509e2defd3ce
DenZone1 <dissap95-d@yandex.ru>
2022-07-10 16:13:10 +0400

    Расширяем функциональность скрипта

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -2,14 +2,23 @@
     _properties: {
         getCartViewLink: "",
         addToCartLink: "",
+        decrementLink: "",
+        removeFromCartLink: ""
        
     },
 
     init: function (properties) {
         $.extend(Cart._properties, properties);
-        $(".add-to-cart").click(Cart.addToCart);
-
+        
+        Cart.initEvents();
        
+    },
+
+    initEvents: function () {
+        $(".add-to-cart").click(Cart.addToCart);
+        //$(".cart_quantity_up").click(Cart.incrementItem);
+        //$(".cart_quantity_down").click(Cart.decrementItem);
+        //$(".cart_quantity_delete").click(Cart.removeItem);
     },
 
     addToCart: function (e) {
