ef32d4778bda48c64cb5d1ad69221f9045deaddd
DenZone1 <dissap95-d@yandex.ru>
2022-07-11 19:45:43 +0400

    Работа скрипта

--- UI/WebStore/TagHelpers/Paging.cs
+++ UI/WebStore/TagHelpers/Paging.cs
@@ -11,12 +11,13 @@ namespace WebStore.TagHelpers;
 public class Paging : TagHelper
 {
     //private readonly IUrlHelperFactory _UrlHelperFactory;
+
     public PageViewModel PageModel { get; set; } = null!;
 
     public string PageAction { get; set; } = null!;
 
     [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
-    public Dictionary<string, object> PageUrlValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+    public Dictionary<string, object?> PageUrlValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
 
     //[ViewContext, HtmlAttributeNotBound]
     //public ViewContext ViewContext { get; set; } = null!;
@@ -47,14 +48,13 @@ public class Paging : TagHelper
         {
             //PageUrlValues["PageNumber"] = PageNumber;
             //a.Attributes["href"] = Url.Action(PageAction, PageUrlValues);
-
             a.Attributes["href"] = "#";
         }
 
         PageUrlValues["PageNumber"] = PageNumber;
 
         foreach (var (key, value) in PageUrlValues.Select(v => (v.Key, Value: v.Value?.ToString())).Where(v => v.Value?.Length > 0))
-            a.MergeAttribute($"data-{key}", value); 
+            a.MergeAttribute($"data-{key}", value);
 
         li.InnerHtml.AppendHtml(a);
         return li;
